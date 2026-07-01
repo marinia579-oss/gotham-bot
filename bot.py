9d9d33ac3b962f89051fc869d5a37cb1349940dd
marinia579-oss <marinia579@gmail.com>
2026-07-01 23:17:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,40 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from datetime import time
 from zoneinfo import ZoneInfo
+import json
 import os
-import sqlite3
+from datetime import time
 
-TOKEN = os.getenv("TOKEN")
-
-# ---------------- DB ----------------
-conn = sqlite3.connect("users.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY
-)
-""")
-conn.commit()
+TOKEN = "YOUR_BOT_TOKEN"
+USERS_FILE = "users.json"
 
 def save_user(user_id):
-    cursor.execute("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
-    conn.commit()
+    if not os.path.exists(USERS_FILE):
+        with open(USERS_FILE, "w") as f:
+            json.dump([], f)
+
+    with open(USERS_FILE, "r") as f:
+        users = json.load(f)
+
+    if user_id not in users:
+        users.append(user_id)
+
+    with open(USERS_FILE, "w") as f:
+        json.dump(users, f)
 
 def get_users():
-    cursor.execute("SELECT user_id FROM users")
-    return [row[0] for row in cursor.fetchall()]
+    if not os.path.exists(USERS_FILE):
+        return []
 
-# ---------------- BOT ----------------
+    with open(USERS_FILE, "r") as f:
+        return json.load(f)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
     save_user(user_id)
 
     await update.message.reply_text(
-        "🦇 Welcome to Gotham\n\n"
+        "Welcome to Gotham🦇\n\n"
         "You'll receive a Vitamin D reminder\n"
         "on the 11th of every month."
     )
@@ -43,8 +45,9 @@ async def send_gotham_message(context: ContextTypes.DEFAULT_TYPE):
     text = (
         "Gotham needs Batman\n"
         "Batman needs Vitamin D\n"
+        "The fate of Gotham is in your hands..\n"
         "Take your Vitamin D.\n\n"
-        "- Gotham"
+        "-The people of Gotham"
     )
 
     for user_id in users:
@@ -64,4 +67,5 @@ app.job_queue.run_monthly(
 )
 
 print("Bot is running...")
+
 app.run_polling()
